6329758168ca93c634cc5e792488aa6b18af8ac1
Tanu <tanu021202@gmail.com>
2025-07-18 16:50:46 +0530

    vite changes and user , admin auth

---
+++ Server/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+//user schema
+const userSchema = new mongoose.Schema({
+  fullname : {
+    type : String,
+    required : true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: { 
+    type: String,
+    required: true,
+    enum : ['admin', 'user'], // Assuming roles are 'admin' and 'user'
+    default: 'admin', // Default role is 'admin'
+  },
+})
+
+const user = mongoose.model('User', userSchema);
+
+
+module.exports = user;
+
+//joining date 
+// location
+// age > 22
+// course
+
