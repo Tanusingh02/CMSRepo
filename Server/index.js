c71b725bd714006159b71efba9e8bb72f4a0d9ea
Tanvi <tanvi20.sharma01@gmail.com>
2025-07-18 17:18:48 +0530

    pages

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,5 @@
 const express = require ('express');
-<<<<<<< HEAD
+
 const mongoose = require('./config/db');
 require('dotenv').config();
 const connectDB = require('./config/db');
@@ -10,30 +10,9 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded);
 app.use('/api',pagesroute);
-=======
-const connectDb = require('./config/db');
-require('dotenv').config();
-const userRoute = require('./routes/user.route')
-
-const app =express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-
-//mongodb connection
-connectDb();
-app.use('/user',userRoute)
-
-
-
-
->>>>>>> e11747f0d8864995755ab98004053ea79b5d0ba7
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> e11747f0d8864995755ab98004053ea79b5d0ba7
+
