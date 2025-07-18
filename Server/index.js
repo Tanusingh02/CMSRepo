9a18f59968028e08768c8bfdc9bf2d5d680055bc
Tanvi <tanvi20.sharma01@gmail.com>
2025-07-18 17:06:45 +0530

    index changes and gitignore file

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,14 @@
 const express = require ('express');
 const mongoose = require('./config/db');
 require('dotenv').config();
+const connectDB = require('./config/db');
+const pagesroute=require('../Server/routes/pages.routes')
 const app =express();
 
-
-
-
-
-
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded);
+app.use('/api',pagesroute);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
