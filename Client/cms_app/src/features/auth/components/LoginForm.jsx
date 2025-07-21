498eeda676f885aa9ddcdd6109a7bcd7e5dc3f83
ashwini <ashwinirashwini26@gamil.com>
2025-07-21 18:22:02 +0530

    Login form style is done in different file [external css]

--- Client/cms_app/src/features/auth/components/LoginForm.jsx
+++ Client/cms_app/src/features/auth/components/LoginForm.jsx
@@ -1,79 +1,32 @@
-import React,{useState} from 'react';
-import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
 
-function LoginForm({ onLogin }){
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import LoginFormStructure from './LoginFormStructure';
+function LoginForm({ onLogin }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    
-    const handleLogout=()=>{
+
+    // eslint-disable-next-line no-unused-vars
+    const handleLogout = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('user');
-
         navigate('/login');
-    }
+    };
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        onLogin({email,password});
-        
+        onLogin({ email, password });
+    };
 
-    }
-    LoginForm.PropTypes ={
-        onLogin:PropTypes.func.isRequired,
-    }
-
-    return(
-        <div className="d-flex justify-content-center align-items-center" 
-        style={{
-            backgroundColor: "#1f87c2",
-            minHeight:'100vh',
-            minWidth:'100vw'
-            }}>
-                <form onSubmit={handleSubmit} 
-               style={{
-                marginTop:'-300px',
-                width:'350px'
-             }}>
-                <h2 className='text-center' style={{color:'white'}}>DCX CMS</h2>
-                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={(e)=>setEmail(e.target.value)} required autoFocus
-                style={{
-                    height:'52px',
-                    marginBottom:'-1px',
-                    borderBottomRightRadius:'0px',
-                    borderBottomLeftRadius:'0px'
-                    }}/>
-                <input type='password' value={password} className='form-control' placeholder='Password' onChange={(e)=>setPassword(e.target.value)} required autoFocus
-                    style={{
-                    height:'52px',
-                    marginBottom:'-1px',
-                    borderTopRightRadius:'0px',
-                    borderTopLeftRadius:'0px'
-                    }}/>
-                
-                <div className='mb-3 form-check'
-                style={{
-                    marginTop:'10px',
-                    color:'white',
-                    borderRadius:'0px'
-                        
-                }}>
-                    <input type='checkbox' className='form-check-input' id="rememberMe" style={{borderRadius:'0px',height:'13px', width:'13px',marginTop:"7px",boxShadow:'10px'}}/>
-                        <label className='form-check-label' htmlFor='rememberMe'>Remember Me</label>
-                </div>
-                    <button type='submit' className='btn btn-primary w-100'
-                    style={{
-                        backgroundColor:('#2fa4e7'),
-                        height:'60px'
-                    }}
-                    
-                    >Sign in</button>
-
-                </form>
-            </div>
-        
-        
+    return (
+        <LoginFormStructure
+            email={email}
+            password={password}
+            setEmail={setEmail}
+            setPassword={setPassword}
+            handleSubmit={handleSubmit}
+        />
     );
 }
 
